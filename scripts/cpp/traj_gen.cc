bfc845775f17cd8f72f910b74c31bc2ce47e2bda
Thai Duong <duongphuthai@gmail.com>
2024-11-05 09:07:51 -0600

    minor

--- scripts/cpp/traj_gen.cc
+++ scripts/cpp/traj_gen.cc
@@ -33,6 +33,8 @@ static constexpr float planning_time = 1.0;
 // Maximum simplification time
 static constexpr float simplification_time = 1.0;
 
+// 
+
 auto main(int argc, char **) -> int
 {
     // // Build sphere cage environment
@@ -47,15 +49,15 @@ auto main(int argc, char **) -> int
 
     
     // // Get bounds from VAMP Robot information, scale 0/1 config to min/max
-    // static constexpr std::array<float, dimension> zeros = {0., 0., 0., 0., 0., 0., 0.};
-    // static constexpr std::array<float, dimension> ones = {1., 1., 1., 1., 1., 1., 1.};
+    static constexpr std::array<float, dimension> zeros = {0., 0., 0., 0., 0., 0., 0.};
+    static constexpr std::array<float, dimension> ones = {1., 1., 1., 1., 1., 1., 1.};
 
     // auto zero_v = Configuration(zeros);
     // auto one_v = Configuration(ones);
 
     // Robot::scale_configuration(zero_v);
     // Robot::scale_configuration(one_v);
-    std::cout << "Testttttttttttttt" << std::endl;
+    std::cout << "Test polynomial" << std::endl;
     std::vector<vamp::FloatVector<dimension>> coeffs;
     static constexpr std::array<float, dimension> coeff0 = {0., -0.785, 0., -2.356, 0., 1.571, 0.785};
     static constexpr std::array<float, dimension> coeff1 = {2.35, 1., 0., -0.8, 0, 2.5, 0.785};
@@ -65,12 +67,34 @@ auto main(int argc, char **) -> int
     coeffs.emplace_back(vamp::FloatVector<dimension>(coeff2));
     vamp::planning::Polynomial<dimension> traj(coeffs, 2);
     std::cout << "traj(1.0) = " << traj.eval(1.0) << std::endl;
-    auto traj_dev = traj.derivatives();
+    auto traj_dev = traj.derivative();
     std::cout << "traj_dev(1.0) = " << traj_dev.eval(1.0) << std::endl;
     auto traj_int = traj.integral();
     std::cout << "traj_int(1.0) = " << traj_int.eval(1.0) << std::endl;
     auto traj_squared = traj*traj;
     std::cout << "traj_squared(1.0) = " << traj_squared.eval(1.0) << std::endl;
-    std::cout << "Testttttttttttttt Doneeeeeeeee" << std::endl;
+    std::cout << "Test Done\n\n\n\n" << std::endl;
+
+
+    vamp::FloatVector<dimension> y0(start);
+    vamp::FloatVector<dimension> y0_dot(zeros);
+    vamp::FloatVector<dimension> yf(goal);
+    vamp::FloatVector<dimension> yf_dot(ones);
+    float T = 1.0;
+    std::cout << "Test Optimal Trajectory with Fixed T" << std::endl;
+    auto optimal_traj = vamp::planning::opt_traj<dimension>(y0, y0_dot, yf, yf_dot, T);
+    optimal_traj.print();
+    std::cout << "optimal_traj(0.0) = " << optimal_traj.eval(0.0) << std::endl;
+    std::cout << "optimal_traj(1.0) = " << optimal_traj.eval(1.0) << std::endl;
+    auto optimal_traj_dev = optimal_traj.derivative();
+    std::cout << "optimal_traj_dev(0.0) = " << optimal_traj_dev.eval(0.0) << std::endl;
+    std::cout << "optimal_traj_dev(0.5) = " << optimal_traj_dev.eval(0.5) << std::endl;
+    std::cout << "optimal_traj_dev(1.0) = " << optimal_traj_dev.eval(1.0) << std::endl;
+    auto optimal_traj_int = optimal_traj.integral();
+    std::cout << "optimal_traj_int(1.0) = " << optimal_traj_int.eval(1.0) << std::endl;
+    auto optimal_traj_squared = traj*traj;
+    std::cout << "optimal_traj_squared(1.0) = " << optimal_traj_squared.eval(1.0) << std::endl;
+    std::cout << "Test Done" << std::endl;
     return 0;
+    
 }
--- src/impl/vamp/planning/poly.hh
+++ src/impl/vamp/planning/poly.hh
@@ -19,18 +19,26 @@ namespace vamp::planning
             assert(coeffs_.size() == order_ + 1);
         }
         
+        inline auto print() noexcept
+        {
+            for (auto coeff: coeffs)
+            {
+                std::cout << coeff << std::endl;
+            }
+        }
+
         inline auto eval(float t) noexcept
         {
             FloatVector<dim> val = coeffs.at(0);
 
             for(auto i = 1U; i < coeffs.size(); i++)
             {
-                val = val + coeffs.at(i)*t;
+                val = val + coeffs.at(i)*std::pow(t, i);
             }
             return val;
         }
 
-        inline auto derivatives() noexcept
+        inline auto derivative() noexcept
         {
             Coeffs<dim> ret_coeffs;
             for(auto i = 0U; i < coeffs.size(); i++)
@@ -66,4 +74,25 @@ namespace vamp::planning
             return Polynomial<dim>(ret_coeffs, this->order + o.order);
         }
     };
+
+    template <std::size_t dim>
+    inline auto opt_traj(FloatVector<dim> y0,
+                         FloatVector<dim> y0_dot,
+                         FloatVector<dim> yf,
+                         FloatVector<dim> yf_dot,
+                         float T
+                         ) noexcept -> Polynomial<dim>
+    {
+        assert(T>0);
+        Coeffs<dim> ret_coeffs;
+        ret_coeffs.emplace_back(y0);
+        ret_coeffs.emplace_back(y0_dot);
+        auto dT1 = yf - y0 - y0_dot*T;
+        auto dT2 = yf_dot - y0_dot;
+        auto coeff2 = dT1*3/(T*T) - dT2/T;
+        auto coeff3 = dT1*(-2)/(T*T*T) + dT2/(T*T);
+        ret_coeffs.emplace_back(coeff2);
+        ret_coeffs.emplace_back(coeff3);
+        return Polynomial<dim>(ret_coeffs, 3);
+    }
 }
